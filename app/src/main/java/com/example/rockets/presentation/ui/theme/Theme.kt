a0283504ef375f96a86d31f283133132952bb40a
r1n1os <evagoras.frang@gmail.com>
2023-09-29 15:16:02 +0300

    change default colors

--- app/src/main/java/com/example/rockets/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rockets/presentation/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = lightGray,
+    secondary = darkGray,
+    tertiary = gray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = lightGray,
+    secondary = darkGray,
+    tertiary = gray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -57,7 +57,7 @@ fun RocketsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = darkGray.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
