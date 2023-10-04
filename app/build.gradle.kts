f0369a65b58bd1aaaf730603311e425c0ed3762c
r1n1os <evagoras.frang@gmail.com>
2023-10-04 20:29:41 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id ("kotlin-android")
+    id("kotlin-android")
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
     id("com.google.devtools.ksp")
@@ -28,8 +28,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -54,36 +53,34 @@ android {
     buildToolsVersion = "33.0.1"
 }
 
-var hiltVersion by extra("2.46.1")
-var hiltCompilerVersion by extra( "1.0.0")
+var hiltVersion by extra("2.48")
+var hiltCompilerVersion by extra("1.0.0")
 var retrofitVersion by extra("2.9.0")
-val roomVersion by extra ("2.5.2")
-val coroutines by extra("1.7.1")
+val roomVersion by extra("2.5.2")
+val coroutines by extra("1.7.3")
 val coroutinesLifecycle by extra("2.6.2")
-val composeNavigation by extra ("2.7.3")
+val composeNavigation by extra("2.7.4")
 
 dependencies {
     //compose
-    //implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.3")
     implementation("androidx.navigation:navigation-compose:$composeNavigation")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     //Hilt
     implementation("com.google.dagger:hilt-android:$hiltVersion")
-    ksp("com.google.dagger:hilt-android-compiler:2.48")
+    ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
     ksp("androidx.hilt:hilt-compiler:1.0.0")
-    //implementation("androidx.navigation:navigation-runtime-ktx:2.7.2")
 
     //Network Manager
-    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
     //Local database Manager
     implementation("androidx.room:room-runtime:$roomVersion")
@@ -101,13 +98,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     //Glide
-    implementation ("com.github.bumptech.glide:compose:1.0.0-alpha.5")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
 
     // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
